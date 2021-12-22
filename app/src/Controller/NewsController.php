c458e8fbbe5f00dae5deb8a0249cb0aecd3025ca
Andrey Smirnov <s.andrey.d@gmail.com>
2021-12-22 20:31:27 +0300

    Show RBC news in NewsController (/news)

---
+++ app/src/Controller/NewsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\News;
+
+class NewsController extends AbstractController
+{
+    #[Route('/news', name: 'news')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(News::class);
+        $news = $repository->findBy(criteria: [], orderBy: ['id' => 'DESC'], limit: 15);
+
+        foreach ($news as $row) {
+            //cho $row->getName() . "<br />";
+        }
+        //var_dump($news);die();
+
+
+        return $this->render('news/index.html.twig', [
+            'news' => $news,
+        ]);
+    }
+}
